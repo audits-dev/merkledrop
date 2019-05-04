bdedf28d828c347a61cc59875ace936f22805b63
Thomas Hodges <me@thodges.com>
2019-05-04 12:53:51 -0500

    Push to repo

---
+++ truffle-config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  networks: {
+    development: {
+      host: '127.0.0.1',
+      port: 8545,
+      network_id: '*',
+    }
+  },
+  compilers: {
+    solc: {
+      version: '0.5.7',
+      settings: {
+       optimizer: {
+         enabled: true,
+         runs: 200
+        }
+      }
+    }
+  },
+  mocha: {
+    reporter: 'eth-gas-reporter',
+    reporterOptions : {
+      currency: 'USD'
+    }
+  }
+}
